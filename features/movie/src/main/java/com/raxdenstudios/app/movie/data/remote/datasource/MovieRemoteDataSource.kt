e17a4523a6b6d05e0f7aaa055758ab668f8a7018
Ángel Gómez <raxden.dev@gmail.com>
2021-04-13 19:46:33 +0200

    simplify get movies usecase

--- features/movie/src/main/java/com/raxdenstudios/app/movie/data/remote/datasource/MovieRemoteDataSource.kt
+++ features/movie/src/main/java/com/raxdenstudios/app/movie/data/remote/datasource/MovieRemoteDataSource.kt
@@ -1,6 +1,8 @@
 package com.raxdenstudios.app.movie.data.remote.datasource
 
+import com.raxdenstudios.app.account.domain.model.Account
 import com.raxdenstudios.app.movie.data.remote.MovieGateway
+import com.raxdenstudios.app.movie.data.remote.exception.UserNotLoggedException
 import com.raxdenstudios.app.movie.data.remote.mapper.MovieDtoToDomainMapper
 import com.raxdenstudios.app.movie.data.remote.model.MovieDto
 import com.raxdenstudios.app.movie.domain.model.Movie
@@ -34,31 +36,51 @@ internal class MovieRemoteDataSource(
         }
       }
 
-  suspend fun watchList(accountId: String, page: Page): ResultData<PageList<Movie>> =
-    movieGateway.watchList(accountId, page.value)
+  suspend fun movies(
+    searchType: SearchType,
+    account: Account,
+    page: Page,
+  ): ResultData<PageList<Movie>> =
+    when (searchType) {
+      SearchType.NowPlaying -> nowPlaying(page)
+      SearchType.Popular -> popular(page)
+      SearchType.TopRated -> topRated(page)
+      SearchType.Upcoming -> upcoming(page)
+      SearchType.WatchList -> watchlist(account, page)
+    }
+
+  private suspend fun watchlist(
+    account: Account,
+    page: Page
+  ): ResultData<PageList<Movie>> = when (account) {
+    is Account.Guest -> ResultData.Error(UserNotLoggedException())
+    is Account.Logged -> watchList(account, page)
+  }
+
+  private suspend fun watchList(account: Account.Logged, page: Page): ResultData<PageList<Movie>> =
+    movieGateway.watchList(account.credentials.accountId, page.value)
       .map { pageDto -> transformPageData(pageDto) }
       .map { pageList -> markMoviesAsWatched(pageList) }
 
   private fun markMoviesAsWatched(pageList: PageList<Movie>) =
     pageList.copy(items = pageList.items.map { movie -> movie.copy(watchList = true) })
 
-  suspend fun movies(
-    searchType: SearchType,
-    page: Page,
-  ): ResultData<PageList<Movie>> =
-    when (searchType) {
-      SearchType.NowPlaying ->
-        movieGateway.nowPlaying(page.value).map { pageDto -> transformPageData(pageDto) }
-      SearchType.Popular ->
-        movieGateway.popular(page.value).map { pageDto -> transformPageData(pageDto) }
-      SearchType.TopRated ->
-        movieGateway.topRated(page.value).map { pageDto -> transformPageData(pageDto) }
-      SearchType.Upcoming ->
-        movieGateway.upcoming(page.value).map { pageDto -> transformPageData(pageDto) }
-    }
+  private suspend fun upcoming(page: Page): ResultData<PageList<Movie>> =
+    movieGateway.upcoming(page.value)
+      .map { pageDto -> transformPageData(pageDto) }
+
+  private suspend fun topRated(page: Page): ResultData<PageList<Movie>> =
+    movieGateway.topRated(page.value)
+      .map { pageDto -> transformPageData(pageDto) }
+
+  private suspend fun popular(page: Page): ResultData<PageList<Movie>> =
+    movieGateway.popular(page.value)
+      .map { pageDto -> transformPageData(pageDto) }
+
+  private suspend fun nowPlaying(page: Page): ResultData<PageList<Movie>> =
+    movieGateway.nowPlaying(page.value)
+      .map { pageDto -> transformPageData(pageDto) }
 
   private fun transformPageData(pageDto: PageDto<MovieDto>): PageList<Movie> =
-    pageDto.toPageList { movieDtoList ->
-      movieDtoToDomainMapper.transform(movieDtoList)
-    }
+    pageDto.toPageList { movieDtoList -> movieDtoToDomainMapper.transform(movieDtoList) }
 }
