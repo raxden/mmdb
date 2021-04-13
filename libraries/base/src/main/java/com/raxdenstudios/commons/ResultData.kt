9362e81510bee2888562b97ba24d01e830951d1e
Ángel Gómez <raxden.dev@gmail.com>
2021-04-13 13:40:46 +0200

    improve coroutines

--- libraries/base/src/main/java/com/raxdenstudios/commons/ResultData.kt
+++ libraries/base/src/main/java/com/raxdenstudios/commons/ResultData.kt
@@ -6,12 +6,52 @@ sealed class ResultData<out T> {
   data class Error(val throwable: Throwable) : ResultData<Nothing>()
 }
 
-fun <T, R> ResultData<T>.map(transform: (value: T) -> R): ResultData<R> = when (this) {
+fun <T, R> ResultData<T>.map(function: (value: T) -> R): ResultData<R> = when (this) {
   is ResultData.Error -> ResultData.Error(throwable)
-  is ResultData.Success -> ResultData.Success(transform(value))
+  is ResultData.Success -> ResultData.Success(function(value))
 }
+
+suspend fun <T, R> ResultData<T>.coMap(function: suspend (value: T) -> R): ResultData<R> =
+  when (this) {
+    is ResultData.Error -> ResultData.Error(throwable)
+    is ResultData.Success -> ResultData.Success(function(value))
+  }
+
+fun <T, R> ResultData<T>.flatMap(function: (value: T) -> ResultData<R>): ResultData<R> =
+  when (this) {
+    is ResultData.Error -> ResultData.Error(throwable)
+    is ResultData.Success -> function(value)
+  }
+
+suspend fun <T, R> ResultData<T>.coFlatMap(function: suspend (value: T) -> ResultData<R>): ResultData<R> =
+  when (this) {
+    is ResultData.Error -> ResultData.Error(throwable)
+    is ResultData.Success -> function(value)
+  }
 
 fun <T> ResultData<T>.getValueOrNull(): T? = when (this) {
   is ResultData.Error -> null
   is ResultData.Success -> value
 }
+
+fun <T> ResultData<T>.getValueOrDefault(default: T): T = when (this) {
+  is ResultData.Error -> default
+  is ResultData.Success -> value
+}
+
+fun <T> ResultData<T>.fold(onSuccess: (T) -> Unit, onError: (Throwable) -> Unit): Unit =
+  when (this) {
+    is ResultData.Error -> onError(throwable)
+    is ResultData.Success -> onSuccess(value)
+  }
+
+fun <T> ResultData<T>.onSuccess(function: (success: T) -> Unit): ResultData<T> = when (this) {
+  is ResultData.Error -> this
+  is ResultData.Success -> also { function(value) }
+}
+
+fun <T> ResultData<T>.onFailure(function: (failure: Throwable) -> Unit): ResultData<T> =
+  when (this) {
+    is ResultData.Error -> also { function(throwable) }
+    is ResultData.Success -> this
+  }
