7ef389918558af2d55a4126392289c6f0d6aa158
Ángel Gómez <raxden.dev@gmail.com>
2021-04-13 14:26:04 +0200

    fix pager

--- features/movie/src/test/java/com/raxdenstudios/app/movie/data/local/datasource/MovieLocalDataSourceTest.kt
+++ features/movie/src/test/java/com/raxdenstudios/app/movie/data/local/datasource/MovieLocalDataSourceTest.kt
@@ -3,9 +3,13 @@ package com.raxdenstudios.app.movie.data.local.datasource
 import com.raxdenstudios.app.movie.data.local.MovieDao
 import com.raxdenstudios.app.movie.data.local.model.MovieEntity
 import com.raxdenstudios.app.movie.di.movieDataModule
+import com.raxdenstudios.app.movie.domain.model.Movie
 import com.raxdenstudios.app.network.APIDataProvider
 import com.raxdenstudios.app.network.model.APIVersion
 import com.raxdenstudios.app.test.BaseTest
+import com.raxdenstudios.commons.pagination.model.Page
+import com.raxdenstudios.commons.pagination.model.PageList
+import com.raxdenstudios.commons.pagination.model.PageSize
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -33,15 +37,71 @@ internal class MovieLocalDataSourceTest : BaseTest() {
     )
 
   private val dataSource: MovieLocalDataSource by inject()
-  
+
   @Test
-  fun `Given a database empty, When 'isWatchList' with movieId param is called, Then return false`() = testDispatcher.runBlockingTest {
-    coEvery { dao.find(aMovieId) } returns MovieEntity.empty.copy(watchList = false)
+  fun `Given a database empty, When a 'watchList' method is called return a pageList empty`() =
+    testDispatcher.runBlockingTest {
+      coEvery { dao.watchList() } returns emptyList()
+      val aPage = Page(1)
+      val aPageSize = PageSize.defaultSize
 
-    val result = dataSource.isWatchList(aMovieId)
+      val result = dataSource.watchList(aPage, aPageSize)
 
-    assertEquals(false, result)
-  }
+      assertEquals(
+        PageList(emptyList<Movie>(), Page(1)),
+        result
+      )
+    }
+
+  @Test
+  fun `Given a database with results, When a 'watchList' method is called and first page is requested return a pageList with results`() =
+    testDispatcher.runBlockingTest {
+      coEvery { dao.watchList() } returns aMovies
+      val aPage = Page(1)
+      val aPageSize = PageSize(2)
+
+      val result = dataSource.watchList(aPage, aPageSize)
+
+      assertEquals(
+        PageList(
+          listOf(
+            Movie.empty.copy(id = 1L),
+            Movie.empty.copy(id = 2L),
+          ), Page(1)
+        ),
+        result
+      )
+    }
+
+  @Test
+  fun `Given a database with results, When a 'watchList' method is called and second page is requested return a pageList with results`() =
+    testDispatcher.runBlockingTest {
+      coEvery { dao.watchList() } returns aMovies
+      val aPage = Page(2)
+      val aPageSize = PageSize(2)
+
+      val result = dataSource.watchList(aPage, aPageSize)
+
+      assertEquals(
+        PageList(
+          listOf(
+            Movie.empty.copy(id = 3L),
+            Movie.empty.copy(id = 4L),
+          ), Page(2)
+        ),
+        result
+      )
+    }
+
+  @Test
+  fun `Given a database empty, When 'isWatchList' with movieId param is called, Then return false`() =
+    testDispatcher.runBlockingTest {
+      coEvery { dao.find(aMovieId) } returns MovieEntity.empty.copy(watchList = false)
+
+      val result = dataSource.isWatchList(aMovieId)
+
+      assertEquals(false, result)
+    }
 
   @Test
   fun `Given a database, When 'isWatchList' with movieId param is called And this movie exists, Then return true`() = testDispatcher.runBlockingTest {
@@ -54,3 +114,11 @@ internal class MovieLocalDataSourceTest : BaseTest() {
 }
 
 private const val aMovieId = 1L
+private val aMovies = listOf(
+  MovieEntity.empty.copy(id = 1L),
+  MovieEntity.empty.copy(id = 2L),
+  MovieEntity.empty.copy(id = 3L),
+  MovieEntity.empty.copy(id = 4L),
+  MovieEntity.empty.copy(id = 5L),
+  MovieEntity.empty.copy(id = 6L),
+)
