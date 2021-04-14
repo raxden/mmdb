8a1be45087f96e4b652a50620c425ef7f5a720e0
Ángel Gómez <raxden.dev@gmail.com>
2021-04-14 16:38:42 +0200

    paginator integrated in viewmodel

--- features/list/src/main/java/com/raxdenstudios/app/list/view/model/MovieListModel.kt
+++ features/list/src/main/java/com/raxdenstudios/app/list/view/model/MovieListModel.kt
@@ -1,7 +1,22 @@
 package com.raxdenstudios.app.list.view.model
 
 import com.raxdenstudios.app.home.view.model.MovieListItemModel
+import com.raxdenstudios.app.movie.domain.model.SearchType
 
 data class MovieListModel(
-  val movies: List<MovieListItemModel>
-)
+  val searchType: SearchType,
+  val movies: List<MovieListItemModel>,
+) {
+
+  companion object {
+    val empty = MovieListModel(
+      searchType = SearchType.Popular,
+      movies = emptyList()
+    )
+
+    fun withSearchType(searchType: SearchType) = MovieListModel(
+      searchType = searchType,
+      movies = emptyList()
+    )
+  }
+}
