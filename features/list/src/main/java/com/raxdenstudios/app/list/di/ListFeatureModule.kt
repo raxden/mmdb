9fc41a3c920dd66b7152736c2d95fddf434ca279
Ángel Gómez <raxden.dev@gmail.com>
2021-04-14 16:38:42 +0200

    paginator integrated in viewmodel

--- features/list/src/main/java/com/raxdenstudios/app/list/di/ListFeatureModule.kt
+++ features/list/src/main/java/com/raxdenstudios/app/list/di/ListFeatureModule.kt
@@ -1,13 +1,25 @@
 package com.raxdenstudios.app.list.di
 
+import com.raxdenstudios.app.home.view.model.MovieListItemModel
 import com.raxdenstudios.app.list.view.MovieListViewModel
-import com.raxdenstudios.app.list.view.mapper.MovieListModelMapper
+import com.raxdenstudios.commons.pagination.Pagination
+import com.raxdenstudios.commons.pagination.model.Page
+import kotlinx.coroutines.CoroutineScope
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import timber.log.Timber
 
 val listFeatureModule = module {
 
-  factory { MovieListModelMapper(get()) }
+  factory { Pagination.Config.default.copy(initialPage = Page(1)) }
+
+  factory { (coroutineScope: CoroutineScope) ->
+    Pagination<MovieListItemModel>(
+      coroutineScope = coroutineScope,
+      logger = { message -> Timber.d(message) },
+      config = get()
+    )
+  }
 
   viewModel { MovieListViewModel(get(), get()) }
 }
