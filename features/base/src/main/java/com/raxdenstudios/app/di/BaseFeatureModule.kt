2f163ecaaa6aa2d81b028b4565bad7bffe531f75
Ángel Gómez <raxden.dev@gmail.com>
2021-04-16 12:08:04 +0200

    add / remove movies from watchlist

--- features/base/src/main/java/com/raxdenstudios/app/di/BaseFeatureModule.kt
+++ features/base/src/main/java/com/raxdenstudios/app/di/BaseFeatureModule.kt
@@ -1,10 +1,13 @@
 package com.raxdenstudios.app.di
 
 import com.raxdenstudios.app.movie.view.mapper.MovieListItemModelMapper
+import com.raxdenstudios.app.movie.view.model.MovieListItemModel
 import com.raxdenstudios.commons.pagination.Pagination
 import com.raxdenstudios.commons.pagination.model.Page
 import com.raxdenstudios.commons.pagination.model.PageSize
+import kotlinx.coroutines.CoroutineScope
 import org.koin.dsl.module
+import timber.log.Timber
 
 val baseFeatureModule = module {
 
@@ -15,6 +18,14 @@ val baseFeatureModule = module {
       initialPage = Page(1),
       pageSize = PageSize(20),
       prefetchDistance = 4
+    )
+  }
+
+  factory { (coroutineScope: CoroutineScope) ->
+    Pagination<MovieListItemModel>(
+      config = get(),
+      logger = { message -> Timber.tag("Pagination").d(message) },
+      coroutineScope = coroutineScope
     )
   }
 }
--- features/list/src/main/java/com/raxdenstudios/app/list/view/model/MovieListModel.kt
+++ features/list/src/main/java/com/raxdenstudios/app/list/view/model/MovieListModel.kt
@@ -2,11 +2,16 @@ package com.raxdenstudios.app.list.view.model
 
 import com.raxdenstudios.app.movie.domain.model.SearchType
 import com.raxdenstudios.app.movie.view.model.MovieListItemModel
+import com.raxdenstudios.commons.ext.replaceItem
 
 data class MovieListModel(
   val searchType: SearchType,
   val movies: List<MovieListItemModel>,
 ) {
+
+  fun replaceMovie(movieListItemModel: MovieListItemModel): MovieListModel = this.copy(
+    movies = this.movies.replaceItem(movieListItemModel) { it.id == movieListItemModel.id }
+  )
 
   companion object {
     val empty = MovieListModel(
