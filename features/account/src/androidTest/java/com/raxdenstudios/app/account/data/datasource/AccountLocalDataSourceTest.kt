6255d903f640ea9771bedc2c7aecb024fa1ff58f
Ángel Gómez <raxden.dev@gmail.com>
2021-04-19 16:18:28 +0200

    observe account implemented

--- features/account/src/androidTest/java/com/raxdenstudios/app/account/data/datasource/AccountLocalDataSourceTest.kt
+++ features/account/src/androidTest/java/com/raxdenstudios/app/account/data/datasource/AccountLocalDataSourceTest.kt
@@ -1,19 +1,34 @@
 package com.raxdenstudios.app.account.data.datasource
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.raxdenstudios.app.account.data.local.AccountDatabase
 import com.raxdenstudios.app.account.data.local.datasource.AccountLocalDataSource
 import com.raxdenstudios.app.account.di.accountDataModule
 import com.raxdenstudios.app.account.domain.model.Account
 import com.raxdenstudios.app.test.BaseAndroidTest
-import kotlinx.coroutines.runBlocking
+import com.raxdenstudios.commons.test.rules.CoroutineTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.koin.core.inject
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
+@ExperimentalCoroutinesApi
 internal class AccountLocalDataSourceTest : BaseAndroidTest() {
+
+  private val testDispatcher = TestCoroutineDispatcher()
+
+  @get:Rule
+  val instantExecutorRule = InstantTaskExecutorRule()
+
+  @get:Rule
+  val coroutineTestRule = CoroutineTestRule(testDispatcher)
 
   private val database: AccountDatabase = AccountDatabase.switchToInMemory(context)
 
@@ -33,11 +48,18 @@ internal class AccountLocalDataSourceTest : BaseAndroidTest() {
   }
 
   @Test
-  fun given_an_empty_database_When_getAccount_is_called_Then_create_guest_account_and_return() {
-    runBlocking {
+  fun given_an_empty2_database_When_getAccount_is_called_Then_create_guest_account_and_return() =
+    testDispatcher.runBlockingTest {
+      val flow = dataSource.observe()
+
+      assertEquals(Account.Guest.default, flow.first())
+    }
+
+  @Test
+  fun given_an_empty_database_When_getAccount_is_called_Then_create_guest_account_and_return() =
+    testDispatcher.runBlockingTest {
       val account = dataSource.getAccount()
 
       assertEquals(Account.Guest.default, account)
     }
-  }
 }
--- features/account/src/main/java/com/raxdenstudios/app/account/data/local/datasource/AccountLocalDataSourceImpl.kt
+++ features/account/src/main/java/com/raxdenstudios/app/account/data/local/datasource/AccountLocalDataSourceImpl.kt
@@ -4,6 +4,10 @@ import com.raxdenstudios.app.account.data.local.AccountDao
 import com.raxdenstudios.app.account.data.local.mapper.AccountEntityToDomainMapper
 import com.raxdenstudios.app.account.data.local.mapper.AccountToEntityMapper
 import com.raxdenstudios.app.account.domain.model.Account
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 internal class AccountLocalDataSourceImpl(
   private val dao: AccountDao,
@@ -11,13 +15,16 @@ internal class AccountLocalDataSourceImpl(
   private val accountToEntityMapper: AccountToEntityMapper,
 ) : AccountLocalDataSource {
 
+  override fun observe(): Flow<Account> = flow {
+    val accountEntity = dao.get()
+    if (accountEntity == null) createGuestAccount()
+    emitAll(observeAccount())
+  }
+
   override suspend fun getAccount(): Account {
     val accountEntity = dao.get()
-    return if (accountEntity == null) {
-      createGuestAccount()
-    } else {
-      accountEntityToDomainMapper.transform(accountEntity)
-    }
+    return if (accountEntity == null) createGuestAccount()
+    else accountEntityToDomainMapper.transform(accountEntity)
   }
 
   override suspend fun createAccount(account: Account) {
@@ -33,4 +40,7 @@ internal class AccountLocalDataSourceImpl(
     createAccount(account)
     return getAccount()
   }
+
+  private fun observeAccount(): Flow<Account> =
+    dao.observe().map { entity -> accountEntityToDomainMapper.transform(entity) }
 }
