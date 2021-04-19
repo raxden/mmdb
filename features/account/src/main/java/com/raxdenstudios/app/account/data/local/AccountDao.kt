28311de80e80b52ddaacfcfcdbeb8b6f9c17bdf7
Ángel Gómez <raxden.dev@gmail.com>
2021-04-19 16:18:28 +0200

    observe account implemented

--- features/account/src/main/java/com/raxdenstudios/app/account/data/local/AccountDao.kt
+++ features/account/src/main/java/com/raxdenstudios/app/account/data/local/AccountDao.kt
@@ -5,9 +5,13 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.raxdenstudios.app.account.data.local.model.AccountEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AccountDao {
+
+  @Query("SELECT * FROM account WHERE id == 1")
+  fun observe(): Flow<AccountEntity>
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
   suspend fun insert(account: AccountEntity)
--- features/base/src/main/java/com/raxdenstudios/app/account/data/local/datasource/AccountLocalDataSource.kt
+++ features/base/src/main/java/com/raxdenstudios/app/account/data/local/datasource/AccountLocalDataSource.kt
@@ -1,8 +1,10 @@
 package com.raxdenstudios.app.account.data.local.datasource
 
 import com.raxdenstudios.app.account.domain.model.Account
+import kotlinx.coroutines.flow.Flow
 
 interface AccountLocalDataSource {
+  fun observe(): Flow<Account>
   suspend fun getAccount(): Account
   suspend fun createAccount(account: Account)
 }
