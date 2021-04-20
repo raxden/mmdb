ee6587b1523d9a40f95489f5be65e36b545ea170
Ángel Gómez <raxden.dev@gmail.com>
2021-04-20 11:48:14 +0200

    add category param to gateway to retrieve movies or tv shows

--- features/movie/src/test/java/com/raxdenstudios/app/movie/data/remote/MovieGatewayTest.kt
+++ features/movie/src/test/java/com/raxdenstudios/app/movie/data/remote/MovieGatewayTest.kt
@@ -11,7 +11,7 @@ import com.raxdenstudios.commons.ResultData
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
-import kotlinx.coroutines.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -25,9 +25,9 @@ internal class MovieGatewayTest : BaseTest() {
 
   private val movieV3Service: MovieV3Service = mockk(relaxed = true)
   private val movieV4Service: MovieV4Service = mockk(relaxed = true) {
-    coEvery { watchList(aAccountId, 1) } returns aNetworkResponseSuccessFirstPage
-    coEvery { watchList(aAccountId, 2) } returns aNetworkResponseSuccessSecondPage
-    coEvery { watchList(aAccountId, 3) } returns aNetworkResponseSuccessThirdPage
+    coEvery { watchList(aAccountId, aCategory, 1) } returns aNetworkResponseSuccessFirstPage
+    coEvery { watchList(aAccountId, aCategory, 2) } returns aNetworkResponseSuccessSecondPage
+    coEvery { watchList(aAccountId, aCategory, 3) } returns aNetworkResponseSuccessThirdPage
   }
 
   private val gateway: MovieGateway by lazy {
@@ -42,29 +42,31 @@ internal class MovieGatewayTest : BaseTest() {
   }
 
   @Test
-  fun `Given a movie results split in pages, When watchList is called with a valid accountId, Then return all movies`() = testDispatcher.runBlockingTest {
-    val resultData = gateway.watchList(aAccountId)
+  fun `Given a movie results split in pages, When watchList is called with a valid accountId, Then return all movies`() =
+    testDispatcher.runBlockingTest {
+      val resultData = gateway.watchList(aCategory, aAccountId)
 
-    coVerify {
-      movieV4Service.watchList(aAccountId, 1)
-      movieV4Service.watchList(aAccountId, 2)
-      movieV4Service.watchList(aAccountId, 3)
+      coVerify {
+        movieV4Service.watchList(aAccountId, aCategory, 1)
+        movieV4Service.watchList(aAccountId, aCategory, 2)
+        movieV4Service.watchList(aAccountId, aCategory, 3)
+      }
+      assertEquals(
+        ResultData.Success(
+          listOf(
+            MovieDto.empty.copy(id = 1),
+            MovieDto.empty.copy(id = 2),
+            MovieDto.empty.copy(id = 3),
+            MovieDto.empty.copy(id = 4),
+            MovieDto.empty.copy(id = 5),
+            MovieDto.empty.copy(id = 6),
+          )
+        ), resultData
+      )
     }
-    assertEquals(
-      ResultData.Success(
-        listOf(
-          MovieDto.empty.copy(id = 1),
-          MovieDto.empty.copy(id = 2),
-          MovieDto.empty.copy(id = 3),
-          MovieDto.empty.copy(id = 4),
-          MovieDto.empty.copy(id = 5),
-          MovieDto.empty.copy(id = 6),
-        )
-      ), resultData
-    )
-  }
 }
 
+private const val aCategory = "movie"
 private const val aAccountId = "aAccountId"
 private val aNetworkResponseSuccessFirstPage = NetworkResponse.Success(
   body = PageDto(
