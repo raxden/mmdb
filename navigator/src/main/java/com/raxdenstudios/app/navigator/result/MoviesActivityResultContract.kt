65efbd4c584bed0dcec7f09445c941f426276274
Ángel Gómez <raxden.dev@gmail.com>
2021-04-21 09:49:46 +0200

    rename MovieListActivity to MediaListActivity

--- navigator/src/main/java/com/raxdenstudios/app/navigator/result/MoviesActivityResultContract.kt
+++ navigator/src/main/java/com/raxdenstudios/app/navigator/result/MoviesActivityResultContract.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContract
-import com.raxdenstudios.app.list.view.MovieListActivity
+import com.raxdenstudios.app.list.view.MediaListActivity
 import com.raxdenstudios.app.list.view.model.MediaListParams
 import com.raxdenstudios.app.movie.view.model.MediaFilterModel
 
@@ -12,7 +12,7 @@ internal class MoviesActivityResultContract : ActivityResultContract<MediaFilter
 
   override fun createIntent(context: Context, mediaFilterModel: MediaFilterModel): Intent {
     val params = MediaListParams(mediaFilterModel)
-    return MovieListActivity.createIntent(context, params)
+    return MediaListActivity.createIntent(context, params)
   }
 
   override fun parseResult(resultCode: Int, intent: Intent?): Boolean =
