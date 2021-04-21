e05e558c4797c661cd363fdda2cda16b9ac7dcb6
Ángel Gómez <raxden.dev@gmail.com>
2021-04-21 12:34:57 +0200

    add to watch list in local

--- features/base/src/main/java/com/raxdenstudios/app/media/domain/AddMediaToWatchListUseCase.kt
+++ features/base/src/main/java/com/raxdenstudios/app/media/domain/AddMediaToWatchListUseCase.kt
@@ -1,10 +1,11 @@
 package com.raxdenstudios.app.media.domain
 
+import com.raxdenstudios.app.media.domain.model.Media
 import com.raxdenstudios.app.media.domain.model.MediaType
 import com.raxdenstudios.commons.ResultData
 
 interface AddMediaToWatchListUseCase {
-  suspend fun execute(params: Params): ResultData<Boolean>
+  suspend fun execute(params: Params): ResultData<Media>
 
   data class Params(
     val mediaId: Long,
