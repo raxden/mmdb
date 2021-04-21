0e8b393ef50df801a5691ea1791b9a9ff6d6cd9a
Ángel Gómez <raxden.dev@gmail.com>
2021-04-21 09:56:10 +0200

    rename MovieListItemModelMapper to MediaListItemModelMapper

--- features/home/src/main/java/com/raxdenstudios/app/home/view/mapper/CarouselMediaListModelMapper.kt
+++ features/home/src/main/java/com/raxdenstudios/app/home/view/mapper/CarouselMediaListModelMapper.kt
@@ -4,12 +4,12 @@ import com.raxdenstudios.app.home.R
 import com.raxdenstudios.app.home.domain.model.HomeModule
 import com.raxdenstudios.app.home.view.model.CarouselMediaListModel
 import com.raxdenstudios.app.movie.domain.model.Media
-import com.raxdenstudios.app.movie.view.mapper.MovieListItemModelMapper
+import com.raxdenstudios.app.movie.view.mapper.MediaListItemModelMapper
 import com.raxdenstudios.commons.provider.StringProvider
 
 internal class CarouselMediaListModelMapper(
   private val stringProvider: StringProvider,
-  private val movieListItemModelMapper: MovieListItemModelMapper,
+  private val mediaListItemModelMapper: MediaListItemModelMapper,
 ) {
 
   fun transform(
@@ -19,27 +19,27 @@ internal class CarouselMediaListModelMapper(
     is HomeModule.NowPlaying -> CarouselMediaListModel(
       label = stringProvider.getString(R.string.home_carousel_now_playing_movies),
       description = "",
-      medias = movieListItemModelMapper.transform(media),
+      medias = mediaListItemModelMapper.transform(media),
     )
     is HomeModule.Popular -> CarouselMediaListModel(
       label = stringProvider.getString(R.string.home_carousel_popular_movies),
       description = "",
-      medias = movieListItemModelMapper.transform(media),
+      medias = mediaListItemModelMapper.transform(media),
     )
     is HomeModule.TopRated -> CarouselMediaListModel(
       label = stringProvider.getString(R.string.home_carousel_top_rated_movies),
       description = "",
-      medias = movieListItemModelMapper.transform(media),
+      medias = mediaListItemModelMapper.transform(media),
     )
     is HomeModule.Upcoming -> CarouselMediaListModel(
       label = stringProvider.getString(R.string.home_carousel_upcoming_movies),
       description = "",
-      medias = movieListItemModelMapper.transform(media),
+      medias = mediaListItemModelMapper.transform(media),
     )
     is HomeModule.WatchList -> CarouselMediaListModel(
       label = stringProvider.getString(R.string.home_carousel_from_your_watchlist),
       description = "",
-      medias = movieListItemModelMapper.transform(media),
+      medias = mediaListItemModelMapper.transform(media),
     )
   }
 }
