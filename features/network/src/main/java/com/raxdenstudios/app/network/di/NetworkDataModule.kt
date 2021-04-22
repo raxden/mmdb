ef4ec0cfec39466aa1602ac2fa75e91a532863dc
Ángel Gómez <raxden.dev@gmail.com>
2021-04-22 16:36:36 +0200

    add gander

--- features/network/src/main/java/com/raxdenstudios/app/network/di/NetworkDataModule.kt
+++ features/network/src/main/java/com/raxdenstudios/app/network/di/NetworkDataModule.kt
@@ -1,15 +1,11 @@
 package com.raxdenstudios.app.network.di
 
 import android.content.Context
+import com.ashokvarma.gander.GanderInterceptor
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.haroldadmin.cnradapter.NetworkResponseAdapterFactory
-import com.raxdenstudios.app.network.APIDataProvider
-import com.raxdenstudios.app.network.APIDataProviderFactory
-import com.raxdenstudios.app.network.AccessTokenInterceptor
-import com.raxdenstudios.app.network.LanguageInterceptor
-import com.raxdenstudios.app.network.SessionInterceptor
-import com.raxdenstudios.app.network.TokenInterceptor
+import com.raxdenstudios.app.network.*
 import com.raxdenstudios.app.network.model.APIVersion
 import com.raxdenstudios.commons.okhttp3.CacheLoggerInterceptor
 import com.raxdenstudios.commons.okhttp3.CacheNetworkInterceptor
@@ -35,7 +31,7 @@ val networkDataModule = module {
   val timeout = 35                  // 35 sec
   val connectionTimeout = 15        // 15 sec
 
-  fun provideHttpClientBuilder(
+  fun provideHttpClientV3Builder(
     cacheLoggerInterceptor: CacheLoggerInterceptor,
     cacheNetworkInterceptor: CacheNetworkInterceptor,
     cacheOfflineInterceptor: CacheOfflineInterceptor,
@@ -43,6 +39,7 @@ val networkDataModule = module {
     languageInterceptor: LanguageInterceptor,
     tokenInterceptor: TokenInterceptor,
     sessionInterceptor: SessionInterceptor,
+    ganderInterceptor: GanderInterceptor,
     cache: Cache
   ): OkHttpClient.Builder = OkHttpClient.Builder()
     .cache(cache)
@@ -53,18 +50,20 @@ val networkDataModule = module {
     .addInterceptor(cacheOfflineInterceptor)
     .addNetworkInterceptor(cacheNetworkInterceptor)
     .addInterceptor(cacheLoggerInterceptor)
+    .addInterceptor(ganderInterceptor)
     .retryOnConnectionFailure(true)
     .readTimeout(timeout.toLong(), TimeUnit.SECONDS)
     .writeTimeout(timeout.toLong(), TimeUnit.SECONDS)
     .connectTimeout(connectionTimeout.toLong(), TimeUnit.SECONDS)
 
-  fun provideHttpClientBuilder(
+  fun provideHttpClientV4Builder(
     cacheLoggerInterceptor: CacheLoggerInterceptor,
     cacheNetworkInterceptor: CacheNetworkInterceptor,
     cacheOfflineInterceptor: CacheOfflineInterceptor,
     httpLoggingInterceptor: HttpLoggingInterceptor,
     languageInterceptor: LanguageInterceptor,
     accessTokenInterceptor: AccessTokenInterceptor,
+    ganderInterceptor: GanderInterceptor,
     cache: Cache
   ): OkHttpClient.Builder = OkHttpClient.Builder()
     .cache(cache)
@@ -74,6 +73,7 @@ val networkDataModule = module {
     .addInterceptor(cacheOfflineInterceptor)
     .addNetworkInterceptor(cacheNetworkInterceptor)
     .addInterceptor(cacheLoggerInterceptor)
+    .addInterceptor(ganderInterceptor)
     .retryOnConnectionFailure(true)
     .readTimeout(timeout.toLong(), TimeUnit.SECONDS)
     .writeTimeout(timeout.toLong(), TimeUnit.SECONDS)
@@ -111,11 +111,12 @@ val networkDataModule = module {
   single { SessionInterceptor(get()) }
   single { AccessTokenInterceptor(get()) }
   single { LanguageInterceptor() }
+  single { GanderInterceptor(get()).apply { showNotification(true) } }
 
   // OKHttpClient ==================================================================================
 
   single(named(APIVersion.V3)) {
-    provideHttpClientBuilder(
+    provideHttpClientV3Builder(
       languageInterceptor = get(),
       tokenInterceptor = get(),
       sessionInterceptor = get(),
@@ -123,18 +124,20 @@ val networkDataModule = module {
       cacheNetworkInterceptor = get(),
       httpLoggingInterceptor = get(),
       cacheLoggerInterceptor = get(),
+      ganderInterceptor = get(),
       cache = get(),
     )
   }
 
   single(named(APIVersion.V4)) {
-    provideHttpClientBuilder(
+    provideHttpClientV4Builder(
       languageInterceptor = get(),
       accessTokenInterceptor = get(),
       cacheOfflineInterceptor = get(),
       cacheNetworkInterceptor = get(),
       httpLoggingInterceptor = get(),
       cacheLoggerInterceptor = get(),
+      ganderInterceptor = get(),
       cache = get(),
     )
   }
