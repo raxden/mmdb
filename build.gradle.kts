99aee484184560e88a58b2020c2c9ac276ea7a1e
Ángel Gómez <raxden.dev@gmail.com>
2021-05-03 21:43:29 +0200

    update configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,10 @@
-buildscript {
-  val kotlin_version by extra("1.4.32")
-  repositories {
-    google()
-    jcenter()
-    mavenCentral()
-    mavenLocal()
-    maven("https://plugins.gradle.org/m2/")
-  }
-  dependencies {
-    classpath("com.google.gms:google-services:4.3.5")
-    classpath("com.google.firebase:firebase-crashlytics-gradle:2.5.2")
-    classpath("com.google.firebase:firebase-appdistribution-gradle:2.1.0")
-    classpath("com.raxdenstudios:android-plugins:0.41")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
-  }
+plugins {
+  id("com.vanniktech.android.junit.jacoco")
+  id("com.raxdenstudios.android-releasing")
 }
 
-plugins {
-  id("com.vanniktech.android.junit.jacoco").version("0.16.0")
-  id("com.raxdenstudios.android-releasing").version("0.41")
+releasing {
+
 }
 
 junitJacoco {
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,19 +3,26 @@ plugins {
 }
 // Required since Gradle 4.10+.
 repositories {
-  mavenCentral()
-  mavenLocal()
   google()
   jcenter()
+  mavenCentral()
+  mavenLocal()
   maven("https://plugins.gradle.org/m2/")
 }
 
 dependencies {
   implementation("com.android.tools.build:gradle:4.1.1")
+  implementation("com.google.gms:google-services:4.3.5")
+  implementation("com.google.firebase:firebase-crashlytics-gradle:2.5.2")
+  implementation("com.google.firebase:firebase-appdistribution-gradle:2.1.1")
+  implementation(kotlin("gradle-plugin", version = "1.4.32"))
+
   implementation("org.ajoberstar.grgit:grgit-gradle:4.1.0")
   implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0")
   implementation("com.adarshr:gradle-test-logger-plugin:3.0.0")
-  implementation(kotlin("gradle-plugin", version = "1.4.32"))
+  implementation("com.vanniktech:gradle-android-junit-jacoco-plugin:0.16.0")
+  implementation("com.raxdenstudios:android-plugins:0.41")
+
   implementation(gradleApi())
   implementation(localGroovy())
 }
