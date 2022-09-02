b1e2e5a1c2c258f97c882da7407241424f5fd00d
Ángel Gómez <angelgomez@xpertai.com>
2022-09-02 21:46:36 +0200

    improving home media

--- features/list/src/main/java/com/raxdenstudios/app/list/view/MediaListActivity.kt
+++ features/list/src/main/java/com/raxdenstudios/app/list/view/MediaListActivity.kt
@@ -16,7 +16,7 @@ import com.raxdenstudios.app.list.view.model.MediaListParams
 import com.raxdenstudios.app.list.view.viewmodel.MediaListViewModel
 import com.raxdenstudios.app.media.view.model.MediaListItemModel
 import com.raxdenstudios.commons.ext.addOnScrolledListener
-import com.raxdenstudios.commons.ext.observe
+import com.raxdenstudios.commons.ext.launchAndCollect
 import com.raxdenstudios.commons.ext.setResultOK
 import com.raxdenstudios.commons.ext.setupToolbar
 import com.raxdenstudios.commons.ext.viewBinding
@@ -27,105 +27,81 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MediaListActivity : BaseActivity() {
 
-  companion object {
-    fun createIntent(context: Context, params: MediaListParams) =
-      Intent(context, MediaListActivity::class.java).apply {
-        putExtra("params", params)
-      }
-  }
-
-  @Inject
-  lateinit var navigator: MediaListNavigator
-
-  @Inject
-  lateinit var errorManager: ErrorManager
-
-  private val binding: MediaListActivityBinding by viewBinding()
-  private val viewModel: MediaListViewModel by viewModels()
-
-  private var onScrolledListener: RecyclerView.OnScrollListener? = null
-  private val adapter: MediaListAdapter by lazy { MediaListAdapter() }
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-
-    binding.setUp()
-
-    observe(viewModel.uiState) { state -> binding.handleState(state) }
-  }
-
-  private fun MediaListActivityBinding.handleState(state: MediaListViewModel.UIState) =
-    when (state) {
-      is MediaListViewModel.UIState.Content -> handleContentState(state)
-      MediaListViewModel.UIState.EmptyContent -> handleEmptyState()
-      is MediaListViewModel.UIState.Error -> handleErrorState(state)
-      MediaListViewModel.UIState.Loading -> handleLoadingState()
+    companion object {
+        fun createIntent(context: Context, params: MediaListParams) =
+            Intent(context, MediaListActivity::class.java).apply {
+                putExtra("params", params)
+            }
     }
 
-  private fun MediaListActivityBinding.handleContentState(state: MediaListViewModel.UIState.Content) {
-    swipeRefreshLayout.isRefreshing = false
+    @Inject
+    lateinit var navigator: MediaListNavigator
 
-    loadMoreMoviesWhenScrollDown()
-    titleValueView.text = state.model.title
-    adapter.populateAdapter(state.model)
-  }
+    @Inject
+    lateinit var errorManager: ErrorManager
 
-  private fun MediaListAdapter.populateAdapter(model: MediaListModel) {
-    submitList(model.items)
-    onMovieClickListener = { TODO() }
-    onAddMovieToWatchListClickListener = { item ->
-      checkIfLoggedAndAddMovieToWatchList(model, item)
+    private val binding: MediaListActivityBinding by viewBinding()
+    private val viewModel: MediaListViewModel by viewModels()
+
+    private var onScrolledListener: RecyclerView.OnScrollListener? = null
+    private val adapter: MediaListAdapter by lazy { MediaListAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.setUp()
+
+        launchAndCollect(viewModel.state) { state -> binding.handleState(state) }
     }
-    onRemoveMovieFromWatchListClickListener = { item ->
-      checkIfLoggedAndRemoveMovieFromWatchList(model, item)
+
+    private fun MediaListActivityBinding.handleState(state: MediaListViewModel.UIState) {
+        swipeRefreshLayout.isRefreshing = state.loading
+        state.error?.let { error -> errorManager.handleError(error) }
+
+        loadMoreMoviesWhenScrollDown()
+        titleValueView.text = state.model.title
+        adapter.populate(state.model)
     }
-  }
 
-  private fun checkIfLoggedAndRemoveMovieFromWatchList(
-    model: MediaListModel,
-    item: MediaListItemModel
-  ) {
-    removeMovieFromWatchList(model, item)
-  }
-
-  private fun removeMovieFromWatchList(model: MediaListModel, item: MediaListItemModel) {
-    this@MediaListActivity.setResultOK()
-    viewModel.removeMovieFromWatchList(model, item)
-  }
-
-  private fun checkIfLoggedAndAddMovieToWatchList(model: MediaListModel, item: MediaListItemModel) {
-    addMovieToWatchList(model, item)
-  }
-
-  private fun addMovieToWatchList(model: MediaListModel, item: MediaListItemModel) {
-    this@MediaListActivity.setResultOK()
-    viewModel.addMovieToWatchList(model, item)
-  }
-
-  private fun MediaListActivityBinding.loadMoreMoviesWhenScrollDown() {
-    onScrolledListener?.run { recyclerView.removeOnScrollListener(this) }
-    onScrolledListener = recyclerView.addOnScrolledListener { _, _, _ ->
-      val gridLayoutManager = recyclerView.layoutManager as GridLayoutManager
-      viewModel.loadMoreMovies(gridLayoutManager.toPageIndex())
+    private fun MediaListAdapter.populate(model: MediaListModel) {
+        submitList(model.items)
+        onMovieClickListener = { TODO() }
+        onAddMovieToWatchListClickListener = { item -> checkIfLoggedAndAddMovieToWatchList(model, item) }
+        onRemoveMovieFromWatchListClickListener = { item -> checkIfLoggedAndRemoveMovieFromWatchList(model, item) }
     }
-  }
 
-  private fun MediaListActivityBinding.handleEmptyState() {
-    swipeRefreshLayout.isRefreshing = false
-  }
+    private fun checkIfLoggedAndRemoveMovieFromWatchList(
+        model: MediaListModel,
+        item: MediaListItemModel,
+    ) {
+        removeMovieFromWatchList(model, item)
+    }
 
-  private fun MediaListActivityBinding.handleErrorState(state: MediaListViewModel.UIState.Error) {
-    swipeRefreshLayout.isRefreshing = false
-    errorManager.handleError(state.throwable)
-  }
+    private fun removeMovieFromWatchList(model: MediaListModel, item: MediaListItemModel) {
+        this@MediaListActivity.setResultOK()
+        viewModel.removeMovieFromWatchList(model, item)
+    }
 
-  private fun MediaListActivityBinding.handleLoadingState() {
-    swipeRefreshLayout.isRefreshing = true
-  }
+    private fun checkIfLoggedAndAddMovieToWatchList(model: MediaListModel, item: MediaListItemModel) {
+        addMovieToWatchList(model, item)
+    }
 
-  private fun MediaListActivityBinding.setUp() {
-    setupToolbar(toolbarView)
-    recyclerView.adapter = adapter
-    swipeRefreshLayout.setOnRefreshListener { viewModel.refreshMovies() }
-  }
+    private fun addMovieToWatchList(model: MediaListModel, item: MediaListItemModel) {
+        this@MediaListActivity.setResultOK()
+        viewModel.addMovieToWatchList(model, item)
+    }
+
+    private fun MediaListActivityBinding.loadMoreMoviesWhenScrollDown() {
+        onScrolledListener?.run { recyclerView.removeOnScrollListener(this) }
+        onScrolledListener = recyclerView.addOnScrolledListener { _, _, _ ->
+            val gridLayoutManager = recyclerView.layoutManager as GridLayoutManager
+            viewModel.loadMoreMovies(gridLayoutManager.toPageIndex())
+        }
+    }
+
+    private fun MediaListActivityBinding.setUp() {
+        setupToolbar(toolbarView)
+        recyclerView.adapter = adapter
+        swipeRefreshLayout.setOnRefreshListener { viewModel.refreshMovies() }
+    }
 }
