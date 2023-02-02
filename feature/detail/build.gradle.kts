84e80f7a28e0e21139e35fd0f9de694bc25a184a
Ángel Gómez <raxden.dev@gmail.com>
2023-02-02 13:22:28 +0100

    fix HomeDataModuleLocalDataSourceTest

--- feature/detail/build.gradle.kts
+++ feature/detail/build.gradle.kts
@@ -86,7 +86,6 @@ dependencies {
 
     // instrumental test libraries
     androidTestImplementation(project(Modules.coreTest))
-    androidTestImplementation(platform(libs.compose.bom))
     androidTestImplementation(libs.bundles.testingUI)
     kaptAndroidTest(libs.hilt.compiler)
 }
