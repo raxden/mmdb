621202ae02faf76cb5d8df0575181719415b7bb6
Ángel Gómez <raxden.dev@gmail.com>
2023-02-07 17:41:45 +0100

    upload reports to ci

--- feature/account/build.gradle.kts
+++ feature/account/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
-    alias(libs.plugins.hilt.android)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
