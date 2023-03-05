7955b3176798ee41c825ae6b3ed34b8557a329ac
Ángel Gómez <raxden.dev@gmail.com>
2023-03-05 18:42:13 +0100

    MainViewModel covered by tests

---
+++ app/src/test/java/com/raxdenstudios/app/ui/MainViewModelTest.kt
@@ -0,0 +1,86 @@
+package com.raxdenstudios.app.ui
+
+import app.cash.turbine.test
+import com.google.common.truth.Truth.assertThat
+import com.raxdenstudios.app.core.navigation.HomeRoutes
+import com.raxdenstudios.app.core.navigation.MainRoutes
+import com.raxdenstudios.commons.test.rules.MainDispatcherRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+internal class MainViewModelTest {
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var viewModel: MainViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = MainViewModel()
+    }
+
+    @Test
+    fun `showBottomBar when route is home`() = runTest {
+        val route = HomeRoutes.home.value
+
+        viewModel.setCurrentRoute(route)
+
+        viewModel.uiState.test {
+            val uiState = awaitItem()
+
+            assertThat(uiState).isEqualTo(
+                MainContract.UIState().copy(shouldShowBottomBar = true)
+            )
+        }
+    }
+
+    @Test
+    fun `showBottomBar when route is search`() = runTest {
+        val route = MainRoutes.search.value
+
+        viewModel.setCurrentRoute(route)
+
+        viewModel.uiState.test {
+            val uiState = awaitItem()
+
+            assertThat(uiState).isEqualTo(
+                MainContract.UIState().copy(shouldShowBottomBar = true)
+            )
+        }
+    }
+
+    @Test
+    fun `showBottomBar when route is account`() = runTest {
+        val route = MainRoutes.account.value
+
+        viewModel.setCurrentRoute(route)
+
+        viewModel.uiState.test {
+            val uiState = awaitItem()
+
+            assertThat(uiState).isEqualTo(
+                MainContract.UIState().copy(shouldShowBottomBar = true)
+            )
+        }
+    }
+
+    @Test
+    fun `showBottomBar is false when route is other`() = runTest {
+        val route = "other"
+
+        viewModel.setCurrentRoute(route)
+
+        viewModel.uiState.test {
+            val uiState = awaitItem()
+
+            assertThat(uiState).isEqualTo(
+                MainContract.UIState().copy(shouldShowBottomBar = false)
+            )
+        }
+    }
+}
