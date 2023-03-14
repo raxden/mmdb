87067a90c67763d4a0afb36a8178fcb1d3d1dc5d
Ángel Gómez <raxden.dev@gmail.com>
2023-03-14 13:23:10 +0100

    update AGP

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -16,7 +16,6 @@ android {
 
     defaultConfig {
         minSdk = Application.minSdk
-        targetSdk = Application.targetSdk
 
         testInstrumentationRunner = Application.testInstrumentationRunner
         consumerProguardFile("consumer-rules.pro")
