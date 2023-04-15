623c06612205c9f2a0213f836ad9e3168eb9db3e
Ángel Gómez <raxden.dev@gmail.com>
2023-04-15 19:37:28 +0200

    set query column as unique

--- core/database/src/main/java/com/raxdenstudios/app/core/database/model/RecentSearchEntity.kt
+++ core/database/src/main/java/com/raxdenstudios/app/core/database/model/RecentSearchEntity.kt
@@ -2,9 +2,10 @@ package com.raxdenstudios.app.core.database.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "recent_search")
+@Entity(tableName = "recent_search", indices = [Index(value = ["query"], unique = true)])
 data class RecentSearchEntity(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     @ColumnInfo(name = "query") val query: String,
