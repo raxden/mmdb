3dc2c7857a3138809ace275ebfaeae9fca28693d
Ángel Gómez <raxden.dev@gmail.com>
2023-05-08 13:30:31 +0200

    create PictureModelMapper

---
+++ core/ui/src/main/java/com/raxdenstudios/app/core/ui/mapper/PictureModelMapper.kt
@@ -0,0 +1,13 @@
+package com.raxdenstudios.app.core.ui.mapper
+
+import com.raxdenstudios.app.core.model.Picture
+import com.raxdenstudios.commons.DataMapper
+import javax.inject.Inject
+
+class PictureModelMapper @Inject constructor() : DataMapper<Picture, String>() {
+
+    override fun transform(source: Picture): String = when (source) {
+        is Picture.Empty -> ""
+        is Picture.WithImage -> source.thumbnail.url
+    }
+}
